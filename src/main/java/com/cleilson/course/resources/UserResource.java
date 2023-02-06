babef9758eaf4e1b9514cf944092dc1c5852b8b6
cleilsonjose <cleilsonrock@gmail.com>
2023-02-06 09:50:21 -0300

    User entity and resource

---
+++ src/main/java/com/cleilson/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.cleilson.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cleilson.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "Israel", "israel@gmail.com", "98741113", "986352");
+		return ResponseEntity.ok().body(u);
+	}
+}
